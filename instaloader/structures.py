f65f897cf105740b586eb1bcc5ac872de517c39f
yolo <omid@sahragard.com>
2020-05-15 01:29:26 +0200

    Business category name gets the 'overall_category_name' instead of the more general one that is quite useless

--- instaloader/structures.py
+++ instaloader/structures.py
@@ -660,7 +660,7 @@ class Profile:
 
     @property
     def business_category_name(self) -> str:
-        return self._metadata('business_category_name')
+        return self._metadata('overall_category_name')
 
     @property
     def biography(self) -> str:
